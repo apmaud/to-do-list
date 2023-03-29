43a5e24c3d2a8bba1349fa68f25afdf4598ec180
Arjun Maudhoo <apm526@nyu.edu>
2023-03-29 19:53:29 -0400

    CreateP roject and Task modules outline

---
+++ src/modules/Project.js
@@ -0,0 +1,57 @@
+import { toDate, isToday, isThisWeek, subDays } from 'date-fns'
+
+export default class Project {
+    constructor(name){
+        this.name = name
+        this.taskList = []
+    }
+    
+    setName(name){
+        this.name = name
+    }
+
+    getName(){
+        return this.name
+    }
+    
+    setTaskList(taskList){
+        this.taskList = taskList
+    }
+
+    getTaskList(){
+        return taskList
+    }
+
+    addTask(newTaskName){
+        if (this.taskList.find((task) => task.getName() === newTaskName.name)) 
+            {return}
+        else 
+            {this.taskList.push(newTaskName)}
+    }
+
+    deleteTask(delTaskName){
+        this.taskList = this.taskList.filter((task) => task.name !== delTaskName)
+    }
+
+    getExactTask(searchTaskName){
+        return this.taskList.find((task) => task.getName() === searchTaskName)
+    }
+
+    getContainsTask(searchTaskName){
+        return this.taskList.some((task) => task.getName() === searchTaskName)
+    }
+
+    getWeeklyTasks(){
+        return this.taskList.filter((task) => {
+            const tDate = new Date(task.getDate())
+            return isThisWeek(subDays(toDate(tDate), 1))
+        })
+    }
+
+    getDailyTasks(){
+        return this.taskList.filter((task) => {
+            const tDate = new Date(task.getDate())
+            return isToday(toDate(tDate))
+        })
+    }
+}
--- src/modules/Task.js
+++ src/modules/Task.js
@@ -1,7 +1,23 @@
 export default class Task {
-    constructor(name, dueDate) {
+    constructor(name, dueDate = 'No Date') {
         this.name = name;
-        
+        this.dueDate = dueDate;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+    
+    getName() {
+        return this.name;
+    }
+
+    setDate(dueDate){
+        this.dueDate = dueDate;
+    }
+
+    getDate(){
+        return this.dueDate;
     }
 
 }
